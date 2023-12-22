caf3c7192b0b6211cf562654a720f9d30154c2ef
Pavel <web.zaporozhtsev@gmail.com>
2023-12-22 04:03:44 +0100

    added tasks for testing

--- dist/data/operationDataBase.js
+++ dist/data/operationDataBase.js
@@ -9,6 +9,18 @@ const DataBase = {
             message: 'Fist todo, start your todo below in your task manager',
             isActive: false,
         },
+        {
+            id: 2,
+            userId: 1,
+            message: 'No, it my task',
+            isActive: true,
+        },
+        {
+            id: 3,
+            userId: 1,
+            message: 'Your task below',
+            isActive: false,
+        },
     ],
 };
 exports.DataBase = DataBase;
